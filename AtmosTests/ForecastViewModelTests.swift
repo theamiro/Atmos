904677bae7416fde46b1db2451aecb5c759593f0
Michael Amiro <odindo.michael@gmail.com>
2025-01-18 08:40:13 +0300

    Define unit tests

---
+++ AtmosTests/ForecastViewModelTests.swift
@@ -0,0 +1,32 @@
+//
+//  ForecastViewModelTests.swift
+//  AtmosTests
+//
+//  Created by Michael Amiro on 16/01/2025.
+//
+
+import XCTest
+@testable import Atmos
+import Foundation
+
+final class ForecastViewModelTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+    }
+
+    func testGetCurrentWeather() {
+
+    }
+
+    func testGet5DayForecast() {
+
+    }
+
+    func testAddFavorite() {
+
+    }
+
+    func testRemoveFavorite() {
+
+    }
+}
